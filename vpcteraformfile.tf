e421b3f594cbfd8a3377a9364e4d4c2c2be4c7c7
root <root@ip-172-31-5-17.ap-south-1.compute.internal>
2024-07-11 16:25:36 +0000

    srinu: az details changed to india

--- vpcteraformfile.tf
+++ vpcteraformfile.tf
@@ -6,7 +6,7 @@ module "vpc" {
   # VPC Basic Details
   name = "vpc-dev"
   cidr = "10.0.0.0/16"   
-  azs                 = ["us-east-2a", "us-east-2b"]
+  azs                 = ["ap-south-1a", "ap-south-2b"]
   private_subnets     = ["10.0.1.0/24"]
   public_subnets      = ["10.0.101.0/24"]
 
